5cd3a9e112ba6a8e9a5fe8dd69198a88145980a0
likeyoucan <122109622+likeyoucan@users.noreply.github.com>
2026-04-17 17:00:44 +0300

    upd

--- www/js/theme.js
+++ www/js/theme.js
@@ -21,8 +21,8 @@ export const themeManager = {
   hasVignette: false,
   isLiquidGlass: false,
   hideNavLabels: false,
-  vignetteAlpha: 0.5,
-  vignetteLevels: [0.1, 0.25, 0.5, 0.75, 1],
+ vignetteAlpha: 0.2,
+ vignetteLevels: [0.15, 0.175, 0.2, 0.225, 0.25],
   vignetteLabels: [
     "vignette_min",
     "vignette_low",
@@ -431,7 +431,7 @@ export const themeManager = {
     this.hideNavLabels = safeGetLS("app_hide_nav_labels") === "true";
     const storedVignetteAlpha = safeGetLS("app_vignette_alpha");
     this.vignetteAlpha =
-      storedVignetteAlpha !== null ? parseFloat(storedVignetteAlpha) : 0.5;
+      storedVignetteAlpha !== null ? parseFloat(storedVignetteAlpha) : 0.2;
     this.showMs = safeGetLS("app_show_ms") !== "false";
     this.swMinuteBeep = safeGetLS("app_sw_minute_beep") !== "false";
     this.updateAdaptiveClass();
