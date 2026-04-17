7dab97aa948dbccdb5cca2781a4c05d59f464bd7
likeyoucan <122109622+likeyoucan@users.noreply.github.com>
2026-04-17 16:24:38 +0300

    upd

--- www/js/theme.js
+++ www/js/theme.js
@@ -22,7 +22,7 @@ export const themeManager = {
   isLiquidGlass: false,
   hideNavLabels: false,
   vignetteAlpha: 0.5,
-  vignetteLevels: [0.1, 0.25, 0.5, 0.75, 1],
+  vignetteLevels: [0.05, 0.01, 0.15, 0.2, 0.25],
   vignetteLabels: [
     "vignette_min",
     "vignette_low",
