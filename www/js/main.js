f553506d9c5b0688267154bbb3a789f203da275d
likeyoucan <122109622+likeyoucan@users.noreply.github.com>
2026-04-22 11:47:57 +0300

    upd

--- www/js/main.js
+++ www/js/main.js
@@ -35,10 +35,10 @@ function injectSVG() {
     if (!ringId || container.querySelector("svg")) return;
     const pointerEventsClass = type === "tm" ? "pointer-events-none" : "";
     const svgHTML = `
-      <svg focusable="false" class="w-full h-full transform ${pointerEventsClass}" viewBox="0 0 100 100" aria-hidden="true">
-        <circle class="app-text opacity-10" stroke-width="var(--ring-stroke-width, 4)" stroke="currentColor" fill="transparent" r="45" cx="50" cy="50" />
-        <circle id="${ringId}" class="progress-ring__circle primary-stroke" stroke-width="var(--ring-stroke-width, 4)" stroke-linecap="round" fill="transparent" r="45" cx="50" cy="50" />
-      </svg>`;
+  <svg focusable="false" class="w-full h-full transform ${pointerEventsClass}" viewBox="0 0 100 100" aria-hidden="true">
+    <circle class="app-text opacity-10 transition-all duration-300 group-focus-visible:primary-text group-focus-visible:opacity-30" stroke-width="var(--ring-stroke-width, 4)" stroke="currentColor" fill="transparent" r="45" cx="50" cy="50" />
+    <circle id="${ringId}" class="progress-ring__circle primary-stroke" stroke-width="var(--ring-stroke-width, 4)" stroke-linecap="round" fill="transparent" r="45" cx="50" cy="50" />
+  </svg>`;
     container.insertAdjacentHTML("afterbegin", svgHTML);
   });
 }
