5cd9ef33160ace167d5394be356864923c69d754
likeyoucan <122109622+likeyoucan@users.noreply.github.com>
2026-04-26 11:45:24 +0300

    upd

--- www/js/stopwatch.js
+++ www/js/stopwatch.js
@@ -74,8 +74,8 @@ const stopwatchModule = {
     );
 
     const sortOptions = [
-      { value: "date_asc", text: t("date_old") },
       { value: "date_desc", text: t("date_new") },
+      { value: "date_asc", text: t("date_old") },
       { value: "result_fast", text: t("result_fast") },
       { value: "name_az", text: t("name_az") },
       { value: "name_za", text: t("name_za") },
