52ab0de0e78e75a54d0a1d096ce8e06dca0828b8
likeyoucan <122109622+likeyoucan@users.noreply.github.com>
2026-05-23 15:08:53 +0300

    upd

--- www/js/app-icon-selector.js
+++ www/js/app-icon-selector.js
@@ -15,18 +15,6 @@ const ICON_OPTIONS = [
     image: "img/app_img.png",
     labelKey: "app_icon_pro",
   },
-    {
-    id: "pro_1",
-    nativeName: "pro_1",
-    image: "img/app_img.png",
-    labelKey: "app_icon_pro",
-  },
-      {
-    id: "pro_123",
-    nativeName: "123",
-    image: "img/app_img.png",
-    labelKey: "app_icon_pro",
-  },
 ];
 
 function getNativePlugin() {
